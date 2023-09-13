c8089d560556d13d1444c6d6d842a0e5f96d8608
Dume Paschal <105561319+paxchal@users.noreply.github.com>
2023-09-13 09:56:20 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Movie from "./Movie";
 import Header from "./components/Header";
-import MenuBar from "./components/MenuBar";
+
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
-      <MenuBar />
+
       <Movie />
       <Footer />
     </div>

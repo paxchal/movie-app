bdf37a9cf58f435e359dda4078f48650c5fa221d
Dume Paschal <105561319+paxchal@users.noreply.github.com>
2023-09-13 22:16:39 +0100

    created the navigation bar

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,12 @@
 // MovieDetails.js
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import logoBlack from "../icons/Logo-black.png";
+import Home from "../icons/Home.png";
+import MovieProjector from "../icons/Movie Projector.png";
+import TvShow from "../icons/TV Show.png";
+import Calendar from "../icons/Calendar.png";
+import Logout from "../icons/Logout.png";
 
 const API_KEY = "768c9eea124bf8e5fb36fd9b917a9974"; // Replace with your API key
 const API_URL = "https://api.themoviedb.org/3/movie/";
@@ -23,11 +29,60 @@ function MovieDetails() {
 
   // Render movie details using the movieDetails object
   return (
-    <div>
-      <h2>{movieDetails.title}</h2>
-      <p>{movieDetails.overview}</p>
-      {/* Add more movie details here */}
-    </div>
+    <section className="section-movie-details">
+      <nav>
+        <section>
+          <img className="nav-logo" src={logoBlack} alt="Movie Logo Black" />
+        </section>
+        <ul>
+          <li>
+            <div>
+              <img className="nav-icons" src={Home} alt="Home Icon" />
+            </div>
+            <p className="nav-name">Home</p>
+          </li>
+          <li>
+            <div>
+              <img
+                className="nav-icons"
+                src={MovieProjector}
+                alt="MovieProjector Icon"
+              />
+            </div>
+            <p className="nav-name">Movies</p>
+          </li>
+          <li>
+            <div>
+              <img className="nav-icons" src={TvShow} alt="TvShow Icon" />
+            </div>
+            <p className="nav-name">TV Series</p>
+          </li>
+          <li>
+            <div>
+              <img className="nav-icons" src={Calendar} alt="Calendar Icon" />
+            </div>
+            <p className="nav-name">Upcoming</p>
+          </li>
+          <div className="nav-cta-cnt">
+            <div className="nav-cta-des-cnt">
+              <p className="des-one">Play movie quizes and earn free tickets</p>
+              <p className="des-two">50k people are playing now</p>
+            </div>
+
+            <div className="nav-cta-btn-cnt">
+              <button className="nav-cta-btn">Start playing</button>
+            </div>
+          </div>
+          <li>
+            <div>
+              <img className="nav-icons" src={Logout} alt="LogoutIcon" />
+            </div>
+            <p className="nav-name">Log out</p>
+          </li>
+        </ul>
+      </nav>
+      <section>DETAILS CONTENT</section>
+    </section>
   );
 }
 

c692c1b71ed15ea971dcbd0e85892a4c2bec7da6
Dume Paschal <105561319+paxchal@users.noreply.github.com>
2023-09-13 19:56:44 +0100

    added a movie detail function

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import React from "react";
-import Movie from "./Movie";
-import Header from "./components/Header";
 
-import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MovieDetails from "./pages/MovieDetails";
+import Home from "./Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Header />
-        <Movie />
-
         <Routes>
           {/* Route for the movie detail page */}
+          <Route index element={<Home />} />
           <Route path="/movie/:movie_id" element={<MovieDetails />} />
         </Routes>
-
-        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Movie from "./Movie";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Movie />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;

7c8dad7c4359a293dbf0d62301f7455382e36746
Dume Paschal <105561319+paxchal@users.noreply.github.com>
2023-09-14 02:01:23 +0100

    updated the ui for the movie details

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,4 +1,3 @@
-// MovieDetails.js
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import logoBlack from "../icons/Logo-black.png";
@@ -8,12 +7,14 @@ import TvShow from "../icons/TV Show.png";
 import Calendar from "../icons/Calendar.png";
 import Logout from "../icons/Logout.png";
 
-const API_KEY = "768c9eea124bf8e5fb36fd9b917a9974"; // Replace with your API key
+const API_KEY = "768c9eea124bf8e5fb36fd9b917a9974";
 const API_URL = "https://api.themoviedb.org/3/movie/";
 
 function MovieDetails() {
   const { movie_id } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
+  const [credits, setCredits] = useState(null);
+  const [trailerThumbnail, setTrailerThumbnail] = useState(null);
 
   useEffect(() => {
     // Fetch movie details using the movie_id and API key
@@ -21,15 +22,37 @@ function MovieDetails() {
       .then((response) => response.json())
       .then((data) => setMovieDetails(data))
       .catch((error) => console.error("Error fetching movie details:", error));
+
+    // Fetch movie credits to get director, writer, and stars
+    fetch(`${API_URL}${movie_id}/credits?api_key=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => setCredits(data))
+      .catch((error) => console.error("Error fetching movie credits:", error));
+
+    // Fetch movie videos to get trailer thumbnail
+    fetch(`${API_URL}${movie_id}/videos?api_key=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const trailer = data.results.find((video) => video.type === "Trailer");
+        if (trailer) {
+          setTrailerThumbnail(
+            `https://img.youtube.com/vi/${trailer.key}/0.jpg`
+          );
+        }
+      })
+      .catch((error) => console.error("Error fetching movie videos:", error));
   }, [movie_id]);
 
-  if (!movieDetails) {
+  if (!movieDetails || !credits || trailerThumbnail === null) {
     return <div>Loading...</div>;
   }
 
-  // Render movie details using the movieDetails object
+  const director = credits.crew.find((person) => person.job === "Director");
+  const writer = credits.crew.find((person) => person.job === "Writer");
+  const stars = credits.cast.slice(0, 5); // Get the top 5 cast members
+
   return (
-    <section className="section-movie-details">
+    <section className="section-movie-nav">
       <nav>
         <section>
           <img className="nav-logo" src={logoBlack} alt="Movie Logo Black" />
@@ -81,7 +104,37 @@ function MovieDetails() {
           </li>
         </ul>
       </nav>
-      <section>DETAILS CONTENT</section>
+      <article>
+        <div className="section-movie-details">
+          <div className="movie-thumbnail">
+            {trailerThumbnail && (
+              <img
+                className="trailer-thumbnail"
+                src={trailerThumbnail}
+                alt="Trailer Thumbnail"
+              />
+            )}
+          </div>
+          <p className="movie-details-title">{movieDetails.title}</p>
+          <p className="movie-detail-rating">
+            Rating: <span>{movieDetails.vote_average} </span>/ 10
+          </p>
+          <div className="movie-desctiption">{movieDetails.overview}</div>
+          <div className="showtimes-btn-cnt">SEE SHOWTIME</div>
+          <div className="more-btn-cnt"> MORE WATCH OPTION</div>
+          <div className="movie-director">
+            DIRECTOR {director ? director.name : "Not available"}
+          </div>
+          <div className="sugessted-movies">IMAGES</div>
+          <div className="movie-writer">
+            Writer: {writer ? writer.name : "Not available"}
+          </div>
+          <div className="movie-stars">
+            Stars: {stars.map((star) => star.name).join(", ")}
+          </div>
+          <div className="movie-top-position">BUTTON</div>
+        </div>
+      </article>
     </section>
   );
 }

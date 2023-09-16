d7c511e340cac0d093facc32f4582a2221c9ed19
Dume Paschal <105561319+paxchal@users.noreply.github.com>
2023-09-16 18:06:52 +0100

    fetched the top_rated movies instead

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,24 +4,27 @@ import MovieCard from "./MovieCard";
 function MovieList() {
   const [movieList, setMovieList] = useState([]);
 
-  const getMovies = () => {
+  const getTopRatedMovies = () => {
+    // Replace 'YOUR_API_KEY' with your actual TMDB API key
     fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
     )
       .then((res) => res.json())
       .then((json) => setMovieList(json.results.slice(0, 10)))
-      .catch((error) => console.error("Error fetching data:", error));
+      .catch((error) =>
+        console.error("Error fetching top-rated movies:", error)
+      );
   };
 
   useEffect(() => {
-    getMovies();
+    getTopRatedMovies();
   }, []);
 
   return (
     <div>
-      <ul className="movie-card-li ">
+      <ul className="movie-card-li">
         {movieList.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} /> // Pass the movie object as a prop
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </ul>
     </div>
